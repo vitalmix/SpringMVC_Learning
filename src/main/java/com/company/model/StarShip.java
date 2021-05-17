b0a3e7cc6f341ca7c9bb48317b5756f36f4e1fa5
Vitalij Stinskij <vstinskij@gmail.com>
2021-05-17 15:03:35 +0300

    add custom error message in messages.properties for starShip num of passengers

--- src/main/java/com/company/model/StarShip.java
+++ src/main/java/com/company/model/StarShip.java
@@ -7,15 +7,16 @@ import java.util.LinkedHashMap;
 public class StarShip implements Transport {
 
     @NotNull(message = "is required")
-    //@Size(min = 3, message = "size of model must be => 3")
-    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only letters")
+    @Size(min = 3, message = "size of model must be => 3")
+    //@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only letters")
     private String model;
     private String weapon;
     private String module;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "value of passengers can't be < 0")
     @Max(value = 10, message = "value of passengers can't be > 10")
-    private int numOfPassengers;
+    private Integer numOfPassengers;
 
     private LinkedHashMap<String, String> weaponOptions;
 
@@ -77,11 +78,11 @@ public class StarShip implements Transport {
         this.additionalModules = additionalModules;
     }
 
-    public int getNumOfPassengers() {
+    public Integer getNumOfPassengers() {
         return numOfPassengers;
     }
 
-    public void setNumOfPassengers(int numOfPassengers) {
+    public void setNumOfPassengers(Integer numOfPassengers) {
         this.numOfPassengers = numOfPassengers;
     }
 }
